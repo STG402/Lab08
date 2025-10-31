0afaf3977be3a45989da1e3fd6b2e75c9cb50159
STG402 <xiankai1@ualberta.ca>
2025-10-30 20:26:03 -0600

    test: add failing tests for hasCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,23 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void testHasCity_trueWhenPresent() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testHasCity_falseWhenAbsent() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        assertFalse(list.hasCity(edmonton));
+    }
 }
+
